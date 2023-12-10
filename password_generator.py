fc9798d0185a011e8d66ce06f024f8bd3598e4e8
CaiYufan-NUS <77770417+CaiYufan-NUS@users.noreply.github.com>
2023-12-10 02:45:23 -0600

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -6,3 +6,5 @@ def password_generator(min_length, max_length):
   password = ''.join(choice(chars) for x in range(randint(min_length, max_length)))
   print('Password: %s' % password)
   return password
+
+password_generator(10, 18)
