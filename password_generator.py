ae7dfa86e4783f132f46695eb6a3d28d649d6312
CaiYufan-NUS <77770417+CaiYufan-NUS@users.noreply.github.com>
2023-12-10 02:47:30 -0600

    randomness for the password generator
    Uses import random for namespace cleanliness
    Uses list instead of string for 'chars' variable in order to shuffle, increases randomness
    Instead of string formatting, uses string concatenation because (currently) it is simpler

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,11 @@
-from random import *
+import random
 import char
 
 def password_generator(min_length, max_length):
   chars = char.get_chars()
-  password = ''.join(choice(chars) for x in range(randint(min_length, max_length)))
-  print('Password: %s' % password)
+  random.shuffle(chars)
+  password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
+  print('Password: ' + password)
   return password
 
 password_generator(10, 18)
