13512e34004b476a703a0173425a05ccfe6ddf15
CaiYufan-NUS <77770417+CaiYufan-NUS@users.noreply.github.com>
2023-12-10 02:23:18 -0600

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,8 @@
 from random import *
+import get_chars.get_chars
+
 def password_generator(min_length, max_length):
+  chars = get_chats()
   password = ''.join(choice(chars) for x in range(randint(min_length, max_length)))
   print('Password: %s' % password)
   return password
